c02b7b39d99126ce25f233a3fbd5eaf849cc66fa
Andris Ganģis <andris.gangis@cubesystems.lv>
2024-07-03 01:39:26 +0300

    Add basic scope to users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -6,12 +6,13 @@ use App\Filament\Pages\CreateUser;
 use App\Filament\Pages\EditUser;
 use App\Filament\Pages\ListUsers;
 use App\Models\User;
-use Filament\Forms\Components\Group;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserResource extends Resource
 {
@@ -25,37 +26,24 @@ class UserResource extends Resource
         return $form
             ->columns(1)
             ->schema([
-                TextInput::make('title')
-                    ->label(__('resources.client.title'))
-                    ->required(),
-
-                Group::make(function () {
-                    return [
-                        TextInput::make('registration_number')->label(__('resources.client.registration_number')),
-                        TextInput::make('vat_number')->label(__('resources.client.vat_number')),
-                    ];
-                })->columns(2),
-
-                TextInput::make('address')->label(__('resources.client.address')),
-
-                Group::make(function () {
-                    return [
-                        TextInput::make('bank_name')->label(__('resources.client.bank_name')),
-                        TextInput::make('bank_swift')->label(__('resources.client.bank_swift')),
-                        TextInput::make('bank_iban')->label(__('resources.client.bank_iban')),
-                    ];
-                })->columns(3),
-
-                TextInput::make('agreement_number')->label(__('resources.client.agreement_number'))
+                TextInput::make('name')->required(),
+                TextInput::make('email')->required(),
+                TextInput::make('password')->password(),
             ]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        $userIds = Filament::getTenant()->users()->pluck('users.id');
+        return parent::getEloquentQuery()->whereIn('id', $userIds);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('title')->label(__('resources.client.title')),
-                Tables\Columns\TextColumn::make('registration_number')->label(__('resources.client.registration_number')),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email')
             ])
             ->filters([
                 //
